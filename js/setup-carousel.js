42ef7fc794a6510e99fd8085d1c27b102ad5da1c
agnaldo26 <agnaldosilva9704@gmail.com>
2021-09-13 12:03:35 -0300

    feito a prateleira de produtos no formato carousel

--- js/setup-carousel.js
+++ js/setup-carousel.js
@@ -1,5 +1,12 @@
-$('.owl-carousel').owlCarousel({
+$('#carousel-banner').owlCarousel({
     loop:true,
     nav:true,
     items: 1,
+})
+
+$('#carousel-shelf').owlCarousel({
+    loop:true,
+    nav:true,
+    items: 3,
+    slideBy: 3,
 })

b088fd6fd00abd37dada6cf174406754b9aae2e6
agnaldo26 <agnaldosilva9704@gmail.com>
2021-09-13 15:32:00 -0300

    feito o news-desk-off e a lógica de cadastro de email no js

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('register-email');
+const newsDeskOff = document.getElementById('news-desk-off');
+const btnSubmit = document.getElementById('submit');
+const inputName = document.getElementById('name')
+const confirmationRegister = document.getElementById('confirmation-register')
+const titleConfirmationRegister = document.getElementById('title-confirmation-register')
+const btnReset = document.getElementById('btn-reset')
+
+
+
+window.onload = ()=>{
+    confirmationRegister.style.removeProperty('display');
+    confirmationRegister.style.display = 'none';
+    newsDeskOff.style.removeProperty('display');
+    newsDeskOff.style.display = 'flex';
+
+
+}
+
+form.addEventListener('submit', (e)=>{
+    titleConfirmationRegister.innerHTML = `Olá ${inputName.value}, seu e-mail foi cadastrado com sucesso!`;
+    newsDeskOff.style.removeProperty('display');
+    newsDeskOff.style.display = 'none';
+    confirmationRegister.style.removeProperty('display');
+    confirmationRegister.style.display = 'flex';
+    e.preventDefault();
+})
+
+btnReset.addEventListener('click', ()=>{
+    confirmationRegister.style.removeProperty('display');
+    confirmationRegister.style.display = 'none';
+    newsDeskOff.style.removeProperty('display');
+    newsDeskOff.style.display = 'flex';
+    window.location = "#register-email"
+})
--- js/setup-carousel.js
+++ js/setup-carousel.js
@@ -2,6 +2,9 @@ $('#carousel-banner').owlCarousel({
     loop:true,
     nav:true,
     items: 1,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplayHoverPause: true,
 })
 
 $('#carousel-shelf').owlCarousel({
@@ -9,4 +12,7 @@ $('#carousel-shelf').owlCarousel({
     nav:true,
     items: 3,
     slideBy: 3,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplayHoverPause: true,
 })
